d754098949060e231504382a30ad12f0f2e062d7
matheusnicolas <matheuspb8@gmail.com>
2021-03-11 10:06:56 -0300

    some service implementation, unit tests and more

--- calculator/src/app/calculator/services/calculator.service.spec.ts
+++ calculator/src/app/calculator/services/calculator.service.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, inject } from '@angular/core/testing';
 
 import { CalculatorService } from './calculator.service';
 
@@ -13,4 +13,29 @@ describe('CalculatorService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('must grant that 1 + 4 = 5',
+    inject([CalculatorService], (service: CalculatorService) => {
+      let sum = service.calculate(1, 4, CalculatorService.SUM);
+      expect(sum).toEqual(5);
+    }));
+
+  it('must grant that 20 - 5 = 15',
+    inject([CalculatorService], (service: CalculatorService) => {
+      let sub = service.calculate(20, 5, CalculatorService.SUBTRACTION);
+      expect(sub).toEqual(15);
+    }))
+
+  it('must grant that 2 * 5 = 10',
+    inject([CalculatorService], (service: CalculatorService) => {
+      let mult = service.calculate(2, 5, CalculatorService.MULTIPLICATION);
+      expect(mult).toEqual(10);
+    }))
+
+    it('must grant that 10 / 5 = 2',
+    inject([CalculatorService], (service: CalculatorService) => {
+      let division = service.calculate(10, 5, CalculatorService.DIVISION);
+      expect(division).toEqual(2);
+    }))
+
 });
--- calculator/src/app/calculator/services/calculator.service.ts
+++ calculator/src/app/calculator/services/calculator.service.ts
@@ -1,3 +1,8 @@
+/** 
+ * @author Matheus N.<matheuspb8@gmail.com>
+ * @since 1.0.0
+*/
+
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +10,40 @@ import { Injectable } from '@angular/core';
 })
 export class CalculatorService {
 
+  static readonly SUM: string = '+';
+  static readonly SUBTRACTION: string = '-';
+  static readonly DIVISION: string = '/';
+  static readonly MULTIPLICATION: string = '*';
+
   constructor() { }
+
+  /**
+   * @param num1 number 
+   * @param num2 number 
+   * @param operation the operation to be executed
+   * @returns number: operation result 
+   */
+  calculate(num1: number, num2: number, operation: string): number {
+    let result: number;
+
+    switch(operation) {
+      case CalculatorService.SUM:
+        result = num1 + num2;
+      break;
+
+      case CalculatorService.SUBTRACTION:
+        result = num1 - num2;
+      break;
+
+      case CalculatorService.DIVISION:
+        result = num1 / num2;
+      break;
+
+      case CalculatorService.MULTIPLICATION:
+        result = num1 * num2;
+      break;
+    }
+
+    return result;
+  }
 }
