4c4239dbd0dced6fe2c8add282fb40b4297579b7
matheusnicolas <matheuspb8@gmail.com>
2021-03-18 09:58:20 -0300

    final project commit, need to fix test problem

--- calculator/src/app/calculator/components/calculator.component.spec.ts
+++ calculator/src/app/calculator/components/calculator.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
 import { CalculatorComponent } from './calculator.component';
+import { CalculatorService } from '../services';
 
 describe('CalculatorComponent', () => {
   let component: CalculatorComponent;
@@ -8,7 +9,8 @@ describe('CalculatorComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CalculatorComponent ]
+      declarations: [ CalculatorComponent],
+      providers: [CalculatorService]
     })
     .compileComponents();
   });
@@ -21,5 +23,27 @@ describe('CalculatorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('deve garantir que 10 + 20 = 30', () => {
+    let btn3 = fixture.debugElement.query(By.css('#btn3'));
+    let btnSum = fixture.debugElement.query(By.css('#btnSoma'));
+    let btn2 = fixture.debugElement.query(By.css('#btn2'));
+    let btnCalculate = fixture.debugElement.query(By.css('#btnCalcular'));
+    let display = fixture.debugElement.query(By.css('#display'));
+
+    btn3.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnSum.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btn2.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    btnCalculate.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    expect(display.nativeElement.value).toEqual('30');
   });
 });
--- calculator/src/app/calculator/components/calculator.component.ts
+++ calculator/src/app/calculator/components/calculator.component.ts
@@ -9,12 +9,82 @@ import { CalculatorService } from '../services';
 })
 export class CalculatorComponent implements OnInit {
 
-  constructor(private calculatorService: CalculatorService) { 
+  private number1: string;
+  private number2: string;
+  private result: number;
+  private operation: string;
 
-    
-  }
+  constructor(private calculatorService: CalculatorService) { }
 
   ngOnInit(): void {
+    this.clear();
   }
 
+  clear(): void {
+    this.number1 = '0';
+    this.number2 = null;
+    this.result = null;
+    this.operation = null;
+  }
+
+  addNumber(number: string): void {
+    if (this.operation === null) {
+      this.number1 = this.concatenateNumber(this.number1, number);
+    } else {
+      this.number2 = this.concatenateNumber(this.number2, number);
+    }
+  }
+
+  concatenateNumber(currentNumber: string, numberConcat: string): string {
+    // case contains only '0' or null, restart the value
+    if (currentNumber === '0' || currentNumber === null) {
+      currentNumber = '';
+    }
+
+    // if the first digit is '.', concatenate '0' before the dot
+    if (numberConcat === '.' && currentNumber === '') {
+      return '0.';
+    }
+
+    // case '.' is typed and contains a '.', just returns
+    if (numberConcat === '.' && currentNumber.indexOf('.') > -1) {
+      return currentNumber;
+    }
+
+    return currentNumber + numberConcat;
+  }
+
+  defineOperation(operation: string): void {
+    if (this.operation === null) {
+      this.operation = operation;
+      return;
+    }
+
+    if (this.number2 !== null) {
+      this.result = this.calculatorService.calculate(parseFloat(this.number1), parseFloat(this.number2), this.operation);
+      this.operation = operation;
+      this.number1 = this.result.toString();
+      this.number2 = null;
+      this.result = null;
+    }
+  }
+
+  calculate(): void {
+    if (this.number2 === null) {
+      return;
+    }
+    this.result = this.calculatorService.calculate(parseFloat(this.number1), parseFloat(this.number2), this.operation);
+  }
+
+  get display(): string {
+    if (this.result !== null) {
+      return this.result.toString();
+    }
+
+    if (this.number2 !== null) {
+      return this.number2;
+    }
+
+    return this.number1;
+  }
 }
