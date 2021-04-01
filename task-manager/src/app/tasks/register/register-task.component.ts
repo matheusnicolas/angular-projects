c037f6b61bc0742cc3571bbad7d5ad5dab2ea9f0
matheusnicolas <matheuspb8@gmail.com>
2021-04-01 10:58:58 -0300

    some implementations

--- task-manager/src/app/tasks/register/register-task.component.ts
+++ task-manager/src/app/tasks/register/register-task.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Task, TaskService } from '../shared';
+
 
 @Component({
   selector: 'app-register-task',
@@ -7,9 +11,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterTaskComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTask', {static: true}) formTask: NgForm;
+  task: Task;
+
+  constructor(
+    private taskService: TaskService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.task = new Task();
+  }
+
+  register(): void {
+    if (this.formTask.form.valid) {
+      this.taskService.register(this.task);
+      this.router.navigate(["/tasks"]);
+    }
   }
 
 }
