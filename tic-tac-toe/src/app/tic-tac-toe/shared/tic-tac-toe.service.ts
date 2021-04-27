6a3bea85bd75c0ed806c0e065af55ff5b6312040
matheusnicolas <matheuspb8@gmail.com>
2021-04-27 13:38:30 -0300

    working on service logic for tic tac toe

--- tic-tac-toe/src/app/tic-tac-toe/shared/tic-tac-toe.service.ts
+++ tic-tac-toe/src/app/tic-tac-toe/shared/tic-tac-toe.service.ts
@@ -5,5 +5,59 @@ import { Injectable } from '@angular/core';
 })
 export class TicTacToeService {
 
+  private readonly BOARD_SIZE: number = 3;
+  private readonly X: number = 1;
+  private readonly O: number = 2;
+  private readonly EMPTY: number = 0;
+
+  private board: any;
+  private numMovements: number;
+  private victory: any;
+
+  private _player: number;
+  private _showStart: boolean;
+  private _showBoard: boolean;
+  private _showFinal: boolean;
+
   constructor() { }
+  
+
+  initialize(): void {
+    this._showStart = true;
+    this._showBoard = false;
+    this._showFinal = false;
+    this.numMovements = 0;
+    this._player = this.X;
+    this.victory = false;
+    this.initializeBoard();
+  }
+
+  initializeBoard(): void {
+    this.board = [this.BOARD_SIZE];
+    for (let i = 0; i < this.BOARD_SIZE; i++) {
+      this.board[i] = [this.EMPTY, this.EMPTY, this.EMPTY];
+    }
+  }
+
+  get showStart(): boolean {
+    return this._showStart;
+  }
+
+  get showBoard(): boolean {
+    return this._showBoard;
+  }
+
+  get showFinal(): boolean {
+    return this._showFinal;
+  }
+
+  get player(): number {
+    return this._player;
+  }
+
+  startGame(): void {
+    this._showStart = false;
+    this._showBoard = true;
+  }
+
 }
