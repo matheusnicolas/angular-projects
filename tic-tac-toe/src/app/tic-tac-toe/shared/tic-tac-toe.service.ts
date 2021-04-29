17c8d3c02c5dc6004db787cf017bf2b95952e75b
matheusnicolas <matheuspb8@gmail.com>
2021-04-29 10:29:29 -0300

    more things added

--- tic-tac-toe/src/app/tic-tac-toe/shared/tic-tac-toe.service.ts
+++ tic-tac-toe/src/app/tic-tac-toe/shared/tic-tac-toe.service.ts
@@ -85,7 +85,7 @@ export class TicTacToeService {
     }
   }
 
-  endGame(line: number, column: number, board: any, player: number): void {
+  endGame(line: number, column: number, board: any, player: number) {
     let end: any = false;
 
     if (board[line][0] === player && board[line][1] === player && board[line][2] === player) {
@@ -106,6 +106,82 @@ export class TicTacToeService {
 
     return end;
 
+  }
+
+  cpuPlay(): void {
+    let move: number[] = this.getMove(this.O);
+
+    if (move.length <= 0) {
+      move = this.getMove(this.X);
+    }
+
+    if (move.length <= 0) {
+      let moves: any = [];
+      for (let i = 0; i < this.BOARD_SIZE; i++) {
+        for (let j = 0; j < this.BOARD_SIZE; j++) {
+          if (this.board[i][i] === this.EMPTY) {
+            moves.push([i, j])
+          }
+        }
+      }
+      let k = Math.floor((Math.random() * (moves.length - 1)));
+      move = [moves[k][0], moves[k][1]];
+    }
+    this.board[move[0]][move[1]] = this._player;
+    this.numMovements++;
+    this.victory = this.endGame(move[0], move[1],
+        this.board, this._player);
+    this._player = (this._player === this.X) ? this.O : this.X;
+  }
+
+  getMove(player: number): number[] {
+    let tab = this.board;
+    for (let lin = 0; lin < this.BOARD_SIZE; lin++) {
+      for (let col = 0; col < this.BOARD_SIZE; col++) {
+        if (tab[lin][col] !== this.EMPTY) {
+          continue;
+        }
+        tab[lin][col] = player;
+        if (this.endGame(lin, col, tab, player)) {
+          return [lin, col];
+        }
+        tab[lin][col] = this.EMPTY;
+      }
+    }
+    return [];
+  }
+
+  showX(positionX: number, positionY: number): boolean {
+    return this.board[positionX][positionY] === this.X;
+  }
+
+  showO(positionX: number, positionY: number): boolean {
+    return this.board[positionX][positionY] === this.O;
+  }
+
+  showVictory(positionX: number, positionY: number): boolean {
+    let showVictory: boolean = false;
+
+    if (!this.victory) {
+      return showVictory;
+    }
+
+    for (let position of this.victory) {
+      if (position[0] === positionX && position[1] === positionY) {
+        showVictory = true;
+        break;
+      }
+    }
+
+    return showVictory;
+  }
+
+
+  newJogo(): void {
+    this.initialize();
+    this._showFinal = false;
+    this._showStart = false;
+    this._showBoard = true;
   }
 
 }
