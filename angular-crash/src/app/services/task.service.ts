1228647d4f79c4e50d788fc8155444650d85c142
matheusnicolas <matheuspb8@gmail.com>
2021-06-04 15:15:20 -0300

    services, json mock server and more

---
+++ angular-crash/src/app/services/task.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Task } from '../Task';
+import { TASKS } from '../mock-tasks';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor() { }
+
+  getTasks(): Observable<Task[]> {
+    const tasks = of(TASKS);
+    return tasks;
+  }
+}
